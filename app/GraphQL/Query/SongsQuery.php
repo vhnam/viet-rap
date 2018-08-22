fff72446200f35dda2b0b3619411eba9f1266770
Nam Vo <vhnam2504@gmail.com>
2018-08-22 15:56:02 +0700

    Initial GraphQL

---
+++ app/GraphQL/Query/SongsQuery.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Query;
+
+class SongsQuery extends Query
+{
+
+}
---
+++ app/GraphQL/Type/AlbumsType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\GraphQL\Type;
+
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Type as GraphQLType;
+
+class AlbumsType extends GraphQLType
+{
+
+}
